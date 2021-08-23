7df949ab1ac73758a062c24327c7f8ec6718d2c0
Laura Melissa Guzman <lmguzman@Lauras-MacBook-Pro.local>
2021-08-23 13:23:55 -0700

    added dynamic p2.2

--- figures.R
+++ figures.R
@@ -3,23 +3,26 @@ library(ggplot2)
 library(cowplot)
 library(tidyr)
 library(Metrics)
+library(dplyr)
 
 
 ### main plot code ##
 
-main_plot <- function(case){
+main_plot <- function(case, load_tf, intervals,...){
   
-  output_p <- readRDS(paste0(case,"/outputs/model.summary/all_outputs.rds"))
-  
-  output_p$nyr <- factor(output_p$nyr, levels = c('2', '5', '10'))
-  
-  output_p$sim <- rep(1:(nrow(output_p)/11), each = 11)
-  
-  scenarios <- output_p[term %in% c('p.yr', 'mu.phi.yr', 'mu.gam.yr'), .(term, true_val, sim)] %>% 
-    pivot_wider(names_from = 'term', values_from = 'true_val') 
-  
-  output_p_s <- output_p %>% 
-    left_join(scenarios)
+  if(load_tf == TRUE){
+    output_p <- readRDS(paste0(case,"/outputs/model.summary/all_outputs.rds"))
+    
+    output_p$nyr <- factor(output_p$nyr, levels = c('2', '5', '10'))
+    
+    output_p$sim <- rep(1:(nrow(output_p)/11), each = 11)
+    
+    scenarios <- output_p[term %in% c('p.yr', 'mu.phi.yr', 'mu.gam.yr'), .(term, true_val, sim)] %>% 
+      pivot_wider(names_from = 'term', values_from = 'true_val') 
+    
+    output_p_s <- output_p %>% 
+      left_join(scenarios)
+  }
   
   ## varying detection ##
   
@@ -142,7 +145,7 @@ main_plot <- function(case){
 # visits the same for all species
 # MS_all_all, MS_all_visits vs MS_all_detected 
 
-main_plot('p1')
+main_plot(case = 'p1', load_tf = TRUE)
 
 
 ######## P2 ########
@@ -150,7 +153,8 @@ main_plot('p1')
 # visits are different for all species
 # MS_all_all, MS_all_visits vs MS_all_detected 
 
-main_plot('p2')
+main_plot(case = 'p2', load_tf = TRUE)
+
 
 ######## P2.1 ########
 # All species are everywhere, no ranges
@@ -158,7 +162,7 @@ main_plot('p2')
 # MS_all_all, MS_all_visits vs MS_all_detected 
 # Same as P2 but mu.v.yr = 0 <- the number of visits through time does not decrease
 
-main_plot('p2.1')
+main_plot(case = 'p2.1', load_tf = TRUE)
 
 
 ######## P2.2 ########
@@ -167,11 +171,14 @@ main_plot('p2.1')
 # MS_all_all, MS_all_visits vs MS_all_detected vs MS_all_community
 # MS_all_community is where only community visits are used to infer non-detections
 
+
 case <- 'p2.2'
 
 output_p <- readRDS(paste0(case,"/outputs/model.summary/all_outputs.rds"))
 
 output_p$nyr <- factor(output_p$nyr, levels = c('5', '10'))
+
+output_p$s <- rep(1:(nrow(output_p)/11), each = 11)
 
 output_p %>% head()
 
@@ -191,8 +198,18 @@ summarised_rmse %>%
   facet_grid(nyr~term) +
   theme_cowplot()
 
+##### by prop.visits.same ###
 
+output_p.v.same.0 <- output_p_s[prop.visits.same == 0]
 
+main_plot(case = '2.2_p.v.same.0', load_tf = FALSE, output_p_s = output_p.v.same.0)
 
+output_p.v.same.1 <- output_p_s[prop.visits.same == 1]
 
+main_plot(case = '2.2_p.v.same.1', load_tf = FALSE, output_p_s = output_p.v.same.1)
 
+output_p.v.same.0.5 <- output_p_s[prop.visits.same == 0.5]
+
+main_plot(case = '2.2_p.v.same.0.5', load_tf = FALSE, output_p_s = output_p.v.same.0.5)
+
+output_p_s[estimate > 10]
